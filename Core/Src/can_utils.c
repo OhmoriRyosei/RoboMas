8ae214eaf2334f57959a04b25f98527bbd88838a
OhmoriRyosei <ryosei-ohmori@outlook.jp>
2023-12-20 18:28:26 +0900

    wrote air

--- Core/Src/can_utils.c
+++ Core/Src/can_utils.c
@@ -2,7 +2,7 @@
 // Created by emile on 23/08/18.
 //
 
-//修正必須!!!!!!
+//TODO:修正必須!!!!!!
 
 
 #include <can_utils.h>
@@ -16,110 +16,110 @@ C620_DeviceInfo c620_dev_info_global[8];
 const uint8_t num_of_c620=4;//TODO: 足回りonly
 
 
-actuator_msgs__msg__DeviceInfo CAN_Device_to_DeviceInfo(const CAN_Device* can_device){
-    actuator_msgs__msg__DeviceInfo device;
-    device.node_id = can_device->node_id;
-    device.device_num = can_device->device_num;
-    switch(can_device->node_type){
-        case NODE_MCMD1:
-            device.node_type.node_type = actuator_msgs__msg__NodeType__NODE_MCMD1;
-            break;
-        case NODE_MCMD2:
-            device.node_type.node_type = actuator_msgs__msg__NodeType__NODE_MCMD2;
-            break;
-        case NODE_MCMD3:
-            device.node_type.node_type = actuator_msgs__msg__NodeType__NODE_MCMD3;
-            break;
-        case NODE_MCMD4:
-            device.node_type.node_type = actuator_msgs__msg__NodeType__NODE_MCMD4;
-            break;
-        case NODE_AIR:
-            device.node_type.node_type = actuator_msgs__msg__NodeType__NODE_AIR;
-            break;
-        case NODE_SERVO:
-            device.node_type.node_type = actuator_msgs__msg__NodeType__NODE_SERVO;
-            break;
-        default:
-            break;
-    }
-    return device;
-}
-
-CAN_Device DeviceInfo_to_CAN_Device(const actuator_msgs__msg__DeviceInfo* device_info){
-    CAN_Device can_device;
-    can_device.node_id = device_info->node_id;
-    can_device.device_num = device_info->device_num;
-    switch(device_info->node_type.node_type){
-        case actuator_msgs__msg__NodeType__NODE_MCMD1:
-            can_device.node_type = NODE_MCMD1;
-            break;
-        case actuator_msgs__msg__NodeType__NODE_MCMD2:
-            can_device.node_type = NODE_MCMD2;
-            break;
-        case actuator_msgs__msg__NodeType__NODE_MCMD3:
-            can_device.node_type = NODE_MCMD3;
-            break;
-        case actuator_msgs__msg__NodeType__NODE_MCMD4:
-            can_device.node_type = NODE_MCMD4;
-            break;
-        case actuator_msgs__msg__NodeType__NODE_AIR:
-            can_device.node_type = NODE_AIR;
-            break;
-        case actuator_msgs__msg__NodeType__NODE_SERVO:
-            can_device.node_type = NODE_SERVO;
-            break;
-        default:
-            break;
-    }
-    return can_device;
-}
-
-
-uint8_t MCMD_FB_TYPE_to_ActuatorMsg(MCMD_FB_TYPE fb_type){
-    uint8_t ans = 0;
-    switch(fb_type){
-        case MCMD_FB_DUTY:
-            ans = actuator_msgs__msg__ActuatorFeedback__FB_DUTY;
-            break;
-        case MCMD_FB_POS:
-            ans = actuator_msgs__msg__ActuatorFeedback__FB_POS;
-            break;
-        case MCMD_FB_VEL:
-            ans = actuator_msgs__msg__ActuatorFeedback__FB_VEL;
-            break;
-        default:
-            break;
-    }
-    return ans;
-}
-
-
-actuator_msgs__msg__DeviceInfo C620_Device_to_DeviceInfo(C620_DeviceInfo* c620_device_info){
-    actuator_msgs__msg__DeviceInfo ans;
-    ans.node_type.node_type = actuator_msgs__msg__NodeType__NODE_C620;
-    ans.node_id = actuator_msgs__msg__DeviceInfo__NODE_ID_C620;
-    ans.device_num = c620_device_info->device_id;
-    return ans;
-}
-
-
-actuator_msgs__msg__ActuatorMultipleFeedback Get_C620_ActuatorMultiFB(C620_DeviceInfo* c620_device_info, uint8_t act_fb_type){
-    actuator_msgs__msg__ActuatorMultipleFeedback act_fb;
-    act_fb.device = C620_Device_to_DeviceInfo(c620_device_info);
-    act_fb.fb_type = act_fb_type;
-    act_fb.input = c620_device_info->ctrl_param._req_value;
-    switch (act_fb.fb_type) {
-        case actuator_msgs__msg__ActuatorFeedback__FB_CURRENT:
-            act_fb.output = Get_C620_FeedbackData(c620_device_info).current;
-            break;
-        case actuator_msgs__msg__ActuatorFeedback__FB_POS:
-            act_fb.output = Get_C620_FeedbackData(c620_device_info).position;
-            break;
-        case actuator_msgs__msg__ActuatorFeedback__FB_VEL:
-            act_fb.output = Get_C620_FeedbackData(c620_device_info).velocity;
-            break;
-        default:
-            break;
-    }
-    return act_fb;
-}
+//actuator_msgs__msg__DeviceInfo CAN_Device_to_DeviceInfo(const CAN_Device* can_device){
+//    actuator_msgs__msg__DeviceInfo device;
+//    device.node_id = can_device->node_id;
+//    device.device_num = can_device->device_num;
+//    switch(can_device->node_type){
+//        case NODE_MCMD1:
+//            device.node_type.node_type = actuator_msgs__msg__NodeType__NODE_MCMD1;
+//            break;
+//        case NODE_MCMD2:
+//            device.node_type.node_type = actuator_msgs__msg__NodeType__NODE_MCMD2;
+//            break;
+//        case NODE_MCMD3:
+//            device.node_type.node_type = actuator_msgs__msg__NodeType__NODE_MCMD3;
+//            break;
+//        case NODE_MCMD4:
+//            device.node_type.node_type = actuator_msgs__msg__NodeType__NODE_MCMD4;
+//            break;
+//        case NODE_AIR:
+//            device.node_type.node_type = actuator_msgs__msg__NodeType__NODE_AIR;
+//            break;
+//        case NODE_SERVO:
+//            device.node_type.node_type = actuator_msgs__msg__NodeType__NODE_SERVO;
+//            break;
+//        default:
+//            break;
+//    }
+//    return device;
+//}
+//
+//CAN_Device DeviceInfo_to_CAN_Device(const actuator_msgs__msg__DeviceInfo* device_info){
+//    CAN_Device can_device;
+//    can_device.node_id = device_info->node_id;
+//    can_device.device_num = device_info->device_num;
+//    switch(device_info->node_type.node_type){
+//        case actuator_msgs__msg__NodeType__NODE_MCMD1:
+//            can_device.node_type = NODE_MCMD1;
+//            break;
+//        case actuator_msgs__msg__NodeType__NODE_MCMD2:
+//            can_device.node_type = NODE_MCMD2;
+//            break;
+//        case actuator_msgs__msg__NodeType__NODE_MCMD3:
+//            can_device.node_type = NODE_MCMD3;
+//            break;
+//        case actuator_msgs__msg__NodeType__NODE_MCMD4:
+//            can_device.node_type = NODE_MCMD4;
+//            break;
+//        case actuator_msgs__msg__NodeType__NODE_AIR:
+//            can_device.node_type = NODE_AIR;
+//            break;
+//        case actuator_msgs__msg__NodeType__NODE_SERVO:
+//            can_device.node_type = NODE_SERVO;
+//            break;
+//        default:
+//            break;
+//    }
+//    return can_device;
+//}
+//
+//
+//uint8_t MCMD_FB_TYPE_to_ActuatorMsg(MCMD_FB_TYPE fb_type){
+//    uint8_t ans = 0;
+//    switch(fb_type){
+//        case MCMD_FB_DUTY:
+//            ans = actuator_msgs__msg__ActuatorFeedback__FB_DUTY;
+//            break;
+//        case MCMD_FB_POS:
+//            ans = actuator_msgs__msg__ActuatorFeedback__FB_POS;
+//            break;
+//        case MCMD_FB_VEL:
+//            ans = actuator_msgs__msg__ActuatorFeedback__FB_VEL;
+//            break;
+//        default:
+//            break;
+//    }
+//    return ans;
+//}
+//
+//
+//actuator_msgs__msg__DeviceInfo C620_Device_to_DeviceInfo(C620_DeviceInfo* c620_device_info){
+//    actuator_msgs__msg__DeviceInfo ans;
+//    ans.node_type.node_type = actuator_msgs__msg__NodeType__NODE_C620;
+//    ans.node_id = actuator_msgs__msg__DeviceInfo__NODE_ID_C620;
+//    ans.device_num = c620_device_info->device_id;
+//    return ans;
+//}
+//
+//
+//actuator_msgs__msg__ActuatorMultipleFeedback Get_C620_ActuatorMultiFB(C620_DeviceInfo* c620_device_info, uint8_t act_fb_type){
+//    actuator_msgs__msg__ActuatorMultipleFeedback act_fb;
+//    act_fb.device = C620_Device_to_DeviceInfo(c620_device_info);
+//    act_fb.fb_type = act_fb_type;
+//    act_fb.input = c620_device_info->ctrl_param._req_value;
+//    switch (act_fb.fb_type) {
+//        case actuator_msgs__msg__ActuatorFeedback__FB_CURRENT:
+//            act_fb.output = Get_C620_FeedbackData(c620_device_info).current;
+//            break;
+//        case actuator_msgs__msg__ActuatorFeedback__FB_POS:
+//            act_fb.output = Get_C620_FeedbackData(c620_device_info).position;
+//            break;
+//        case actuator_msgs__msg__ActuatorFeedback__FB_VEL:
+//            act_fb.output = Get_C620_FeedbackData(c620_device_info).velocity;
+//            break;
+//        default:
+//            break;
+//    }
+//    return act_fb;
+//}
--- Core/Src/freertos.c
+++ Core/Src/freertos.c
@@ -25,23 +25,52 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+#include <rcl/rcl.h>
+#include <rcl/error_handling.h>
+#include <rosidl_runtime_c/primitives_sequence_functions.h>
 
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+#include <uxr/client/transport.h>
+#include <rmw_microxrcedds_c/config.h>
+#include <rmw_microros/rmw_microros.h>
+#include <std_msgs/msg/float64.h>
+#include <std_msgs/msg/int32_multi_array.h>
+#include <geometry_msgs/msg/twist.h>
+#include <actuator_status_msg/msg/three_bools.h>
+#include <usart.h>
+
+#include "can_utils.h"
+#include "can.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
 typedef StaticTask_t osStaticThreadDef_t;
+typedef StaticTimer_t osStaticTimerDef_t;
 /* USER CODE BEGIN PTD */
 
 /* USER CODE END PTD */
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
+#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Aborting.\n",__LINE__,(int)temp_rc);vTaskDelete(NULL);}}
+#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Continuing.\n",__LINE__,(int)temp_rc);}}
 
+bool cubemx_transport_open(struct uxrCustomTransport * transport);
+bool cubemx_transport_close(struct uxrCustomTransport * transport);
+size_t cubemx_transport_write(struct uxrCustomTransport* transport, const uint8_t * buf, size_t len, uint8_t * err);
+size_t cubemx_transport_read(struct uxrCustomTransport* transport, uint8_t* buf, size_t len, int timeout, uint8_t* err);
+
+void * microros_allocate(size_t size, void * state);
+void microros_deallocate(void * pointer, void * state);
+void * microros_reallocate(void * pointer, size_t size, void * state);
+void * microros_zero_allocate(size_t number_of_elements, size_t size_of_element, void * state);
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
 /* USER CODE BEGIN PM */
 
+rcl_publisher_t publisher_enc1,publisher_enc2;
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
@@ -60,13 +89,67 @@ const osThreadAttr_t defaultTask_attributes = {
   .stack_size = sizeof(defaultTaskBuffer),
   .priority = (osPriority_t) osPriorityNormal,
 };
+/* Definitions for LEDTask */
+osThreadId_t LEDTaskHandle;
+uint32_t LEDTaskBuffer[ 128 ];
+osStaticThreadDef_t LEDTaskControlBlock;
+const osThreadAttr_t LEDTask_attributes = {
+  .name = "LEDTask",
+  .cb_mem = &LEDTaskControlBlock,
+  .cb_size = sizeof(LEDTaskControlBlock),
+  .stack_mem = &LEDTaskBuffer[0],
+  .stack_size = sizeof(LEDTaskBuffer),
+  .priority = (osPriority_t) osPriorityLow,
+};
+/* Definitions for Timer01 */
+osTimerId_t Timer01Handle;
+osStaticTimerDef_t Timer01ControlBlock;
+const osTimerAttr_t Timer01_attributes = {
+  .name = "Timer01",
+  .cb_mem = &Timer01ControlBlock,
+  .cb_size = sizeof(Timer01ControlBlock),
+};
 
 /* Private function prototypes -----------------------------------------------*/
 /* USER CODE BEGIN FunctionPrototypes */
 
+
+void pub_timer_callback_enc1(rcl_timer_t * timer, int64_t last_call_time){
+    RCLC_UNUSED(last_call_time);
+    std_msgs__msg__Float64 data_msg;
+
+    if (timer != NULL) {
+
+
+
+    }
+}
+
+void subscription_callback(const void * msgin)
+{
+	 // Cast received message to used type
+	  const actuator_status_msg__msg__ThreeBools *status = (const actuator_status_msg__msg__ThreeBools *)msgin;
+	  Air_PortStatus_Typedef air_status[3];
+
+	  air_status[0] = (status.c) ? AIR_ON : AIR_OFF;
+	  air_status[1] = (status.p) ? AIR_ON : AIR_OFF;
+	  air_status[2] = (status.l) ? AIR_ON : AIR_OFF;
+
+	  if(NUM_OF_AIR>0){
+		  for(int i=0;i<NUM_OF_AIR;i++){
+			  for(int j=0;j<3;j++){       //TODO: j<3は手動
+				  air_devices[i].device_num = j;
+				  AirCylinder_SendOutput(&air_devices[i], air_status[i]);
+			  }
+		  }
+	  }
+}
+
 /* USER CODE END FunctionPrototypes */
 
 void StartDefaultTask(void *argument);
+void StartLEDTask(void *argument);
+void Callback01(void *argument);
 
 extern void MX_USB_DEVICE_Init(void);
 void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
@@ -89,6 +172,10 @@ void MX_FREERTOS_Init(void) {
   /* add semaphores, ... */
   /* USER CODE END RTOS_SEMAPHORES */
 
+  /* Create the timer(s) */
+  /* creation of Timer01 */
+  Timer01Handle = osTimerNew(Callback01, osTimerPeriodic, NULL, &Timer01_attributes);
+
   /* USER CODE BEGIN RTOS_TIMERS */
   /* start timers, add new ones, ... */
   /* USER CODE END RTOS_TIMERS */
@@ -100,6 +187,9 @@ void MX_FREERTOS_Init(void) {
   /* Create the thread(s) */
   /* creation of defaultTask */
   defaultTaskHandle = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
+
+  /* creation of LEDTask */
+  LEDTaskHandle = osThreadNew(StartLEDTask, NULL, &LEDTask_attributes);
 
   /* USER CODE BEGIN RTOS_THREADS */
   /* add threads, ... */
@@ -129,6 +219,32 @@ void StartDefaultTask(void *argument)
     osDelay(1);
   }
   /* USER CODE END StartDefaultTask */
+}
+
+/* USER CODE BEGIN Header_StartLEDTask */
+/**
+* @brief Function implementing the LEDTask thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_StartLEDTask */
+void StartLEDTask(void *argument)
+{
+  /* USER CODE BEGIN StartLEDTask */
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END StartLEDTask */
+}
+
+/* Callback01 function */
+void Callback01(void *argument)
+{
+  /* USER CODE BEGIN Callback01 */
+
+  /* USER CODE END Callback01 */
 }
 
 /* Private application code --------------------------------------------------*/
